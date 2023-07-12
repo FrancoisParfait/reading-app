27ff853aed68c33692319ceaf4297c4d136f6d5a
Christian Johnson <cartian4@gmail.com>
2023-07-12 06:16:46 -0400

    Add basic content to DBHandler

--- ReadingApp/app/src/main/java/com/christianjcodes/readingapp/DBHandler.java
+++ ReadingApp/app/src/main/java/com/christianjcodes/readingapp/DBHandler.java
@@ -4,6 +4,20 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DBHandler {
+public class DBHandler extends SQLiteOpenHelper {
 
+    private static final String DB_NAME = "userdb";
+    private static final int DB_VERSION = 1;
+    public DBHandler(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
