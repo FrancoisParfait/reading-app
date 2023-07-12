2f77bc81f981f1a9abaa1db4fe4eae5f832d1573
Christian Johnson <cartian4@gmail.com>
2023-07-12 06:06:24 -0400

    Create DBHandler class for SQLite database

---
+++ ReadingApp/app/src/main/java/com/christianjcodes/readingapp/DBHandler.java
@@ -0,0 +1,9 @@
+package com.christianjcodes.readingapp;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHandler {
+
+}
