17158abb67531d30486af88b7563e8d707f966bc
Christian Johnson <cartian4@gmail.com>
2023-07-13 06:19:03 -0400

    Add columns for userdb

--- ReadingApp/app/src/main/java/com/christianjcodes/readingapp/DBHandler.java
+++ ReadingApp/app/src/main/java/com/christianjcodes/readingapp/DBHandler.java
@@ -9,6 +9,12 @@ public class DBHandler extends SQLiteOpenHelper {
     private static final String DB_NAME = "userdb";
     private static final int DB_VERSION = 1;
     private static final String TABLE_NAME = "users";
+
+    private static final String ID_COL = "id";
+
+    private static final String USERNAME_COL = "username";
+
+    private static final String PASSWORD_COL = "password";
     public DBHandler(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
     }
