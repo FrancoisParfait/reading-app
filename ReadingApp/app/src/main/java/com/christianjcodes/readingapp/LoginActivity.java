b2351f589b9ff598e74cf6cc48c73afe7b6dd7d1
Christian Johnson <cartian4@gmail.com>
2023-07-17 19:15:58 -0400

    Finish LoginActivity onClick function

--- ReadingApp/app/src/main/java/com/christianjcodes/readingapp/LoginActivity.java
+++ ReadingApp/app/src/main/java/com/christianjcodes/readingapp/LoginActivity.java
@@ -2,6 +2,7 @@ package com.christianjcodes.readingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,6 +34,15 @@ public class LoginActivity extends AppCompatActivity {
 
                 if (user.equals("") || pass.equals("")) {
                     Toast.makeText(LoginActivity.this, "Please fill out all the fields!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Boolean checkUserPass = DB.checkUsernamePassword(user, pass);
+                    if (checkUserPass) {
+                        Toast.makeText(LoginActivity.this, "Sign in successful!", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Invalid login.", Toast.LENGTH_SHORT).show();
+                    }
                 }
 
             }
