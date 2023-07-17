78ed571ef367e796a7d2e3dc2111427247bb9330
Christian Johnson <cartian4@gmail.com>
2023-07-17 06:33:54 -0400

    Add username, password, and loginbtn to LoginActivity

--- ReadingApp/app/src/main/java/com/christianjcodes/readingapp/LoginActivity.java
+++ ReadingApp/app/src/main/java/com/christianjcodes/readingapp/LoginActivity.java
@@ -3,12 +3,29 @@ package com.christianjcodes.readingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText username, password;
+    Button loginbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.username1);
+        password = (EditText) findViewById(R.id.password1);
+        loginbtn = (Button) findViewById(R.id.signinbtn1);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
