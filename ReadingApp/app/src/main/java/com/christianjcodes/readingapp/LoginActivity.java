5a4f9302c03722be4989c596f4f185845c9bef0c
Christian Johnson <cartian4@gmail.com>
2023-07-17 19:12:14 -0400

    Add DB to LoginActivity, user and pass variables to onClick function for loginbtn

--- ReadingApp/app/src/main/java/com/christianjcodes/readingapp/LoginActivity.java
+++ ReadingApp/app/src/main/java/com/christianjcodes/readingapp/LoginActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText username, password;
     Button loginbtn;
+    DBHandler DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +22,18 @@ public class LoginActivity extends AppCompatActivity {
         username = (EditText) findViewById(R.id.username1);
         password = (EditText) findViewById(R.id.password1);
         loginbtn = (Button) findViewById(R.id.signinbtn1);
+        DB = new DBHandler(this);
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if (user.equals("") || pass.equals("")) {
+                    Toast.makeText(LoginActivity.this, "Please fill out all the fields!", Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
