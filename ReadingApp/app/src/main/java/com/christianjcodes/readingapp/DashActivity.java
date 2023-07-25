194f02574bfd8c76b2b92ba0251db9878f15c80f
Christian Johnson <cartian4@gmail.com>
2023-07-25 06:24:23 -0400

    Delete DashActivity and replace with a true activity, not just a class

--- ReadingApp/app/src/main/java/com/christianjcodes/readingapp/DashActivity.java
+++ ReadingApp/app/src/main/java/com/christianjcodes/readingapp/DashActivity.java
@@ -1,6 +1,14 @@
 package com.christianjcodes.readingapp;
 
-public class DashActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
 
+public class DashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash);
+    }
 }
