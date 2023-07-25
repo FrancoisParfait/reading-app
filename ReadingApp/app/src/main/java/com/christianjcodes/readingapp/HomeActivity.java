1aad7a64d52a16060ff24ba719b795139e5810e6
Christian Johnson <cartian4@gmail.com>
2023-07-25 06:31:04 -0400

    Add onClick for current_reads, and create CurrentReadsActivity

--- ReadingApp/app/src/main/java/com/christianjcodes/readingapp/HomeActivity.java
+++ ReadingApp/app/src/main/java/com/christianjcodes/readingapp/HomeActivity.java
@@ -10,6 +10,9 @@ import android.view.View;
 public class HomeActivity extends AppCompatActivity {
 
     CardView dashboard;
+    CardView currentReads;
+    CardView completedReads;
+    CardView readingList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +20,22 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         dashboard = (CardView) findViewById(R.id.my_dashboard);
+        currentReads = (CardView) findViewById(R.id.current_reads);
+        completedReads = (CardView) findViewById(R.id.completed_reads);
+        readingList = (CardView) findViewById(R.id.reading_list);
 
         dashboard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), DashActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        currentReads.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CurrentReadsActivity.class);
                 startActivity(intent);
             }
         });
